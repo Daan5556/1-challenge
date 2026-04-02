e9e2fc01740ff8dadba9c72aff80dfd546723145
Daan Eggen <egdaan@gmail.com>
2026-04-02 15:12:34 +0200

    Added seismic measurements

--- sensor-simulation/sensor-simulation/payload.py
+++ sensor-simulation/sensor-simulation/payload.py
@@ -17,7 +17,7 @@ def generate_tablename(sensortype) -> str:
         case 6:
             return "water_quality"
         case 7:
-            return "traffic_camera"
+            return "seismic_sensor"
         case _:
             raise ValueError("Unknown sensortype")
 
@@ -38,7 +38,7 @@ def generate(sensortype) -> str:
         case 6:
             return _generate_water_quality()
         case 7:
-            return _generate_traffic_camera()
+            return _generate_seismic()
         case _:
             raise ValueError("Unknown sensortype")
 
@@ -83,3 +83,11 @@ def _generate_water_quality():
     electrical_conductivity = int(random.gauss(412, 50))
 
     return f"temperature={temperature},ph={ph},turbidity={turbidity},dissolved_oxygen={dissolved_oxygen},electrical_conductivity={electrical_conductivity}"
+
+
+def _generate_seismic():
+    mean = 0.02
+    std_dev = 0.01
+    measurement = max(0, random.gauss(mean, std_dev))
+
+    return f"value={measurement}"
