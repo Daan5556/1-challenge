42834c42757e33a6076911c60442691745354b6c
Daan Eggen <egdaan@gmail.com>
2026-04-02 15:00:05 +0200

    Added sensor-simulation

---
+++ sensor-simulation/sensor-simulation/main.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+
+import os
+import time
+
+import paho.mqtt.client as mqtt
+import payload
+
+MQTT_HOST = os.environ["MQTT_HOST"]
+EDGE_DEVICE_IDENTIFIER = os.environ["EDGE_DEVICE_ID"]
+SENSOR_TYPE = os.environ["SENSOR_TYPE"]
+TABLE_NAME = payload.generate_tablename(SENSOR_TYPE)
+
+MQTT_TOPIC = f"sensors/{EDGE_DEVICE_IDENTIFIER}"
+
+
+def main():
+    mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    mqttc.connect(MQTT_HOST)
+    mqttc.loop_start()
+
+    try:
+        while True:
+            measurements = payload.generate(SENSOR_TYPE)
+            line = (
+                f"{TABLE_NAME},edge_device_id={EDGE_DEVICE_IDENTIFIER} {measurements}"
+            )
+
+            print(line)
+
+            mqttc.publish(MQTT_TOPIC, line)
+            time.sleep(5)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        mqttc.loop_stop()
+        mqttc.disconnect()
+
+
+if __name__ == "__main__":
+    main()
---
+++ sensor-simulation/sensor-simulation/payload.py
@@ -0,0 +1,85 @@
+import random
+
+
+def generate_tablename(sensortype) -> str:
+    sensortype = int(sensortype)
+    match sensortype:
+        case 1:
+            return "traffic_camera"
+        case 2:
+            return "air_quality"
+        case 3:
+            return "noise"
+        case 4:
+            return "temperature_and_humidity"
+        case 5:
+            return "parking_sensor"
+        case 6:
+            return "water_quality"
+        case 7:
+            return "traffic_camera"
+        case _:
+            raise ValueError("Unknown sensortype")
+
+
+def generate(sensortype) -> str:
+    sensortype = int(sensortype)
+    match sensortype:
+        case 1:
+            return _generate_traffic_camera()
+        case 2:
+            return _generate_air_quality()
+        case 3:
+            return _generate_noise()
+        case 4:
+            return _generate_temperature_and_humidity()
+        case 5:
+            return _generate_parking_sensor()
+        case 6:
+            return _generate_water_quality()
+        case 7:
+            return _generate_traffic_camera()
+        case _:
+            raise ValueError("Unknown sensortype")
+
+
+def _generate_traffic_camera():
+    mean = 50
+    std_dev = 10
+    measurement = int(random.gauss(mean, std_dev))
+
+    return f"value={measurement}"
+
+
+def _generate_air_quality():
+    return f"value={1}"
+
+
+def _generate_noise():
+    mean = 60
+    std_dev = 15
+    measurement = int(random.gauss(mean, std_dev))
+
+    return f"value={measurement}"
+
+
+def _generate_temperature_and_humidity():
+    return f"value={1}"
+
+
+def _generate_parking_sensor():
+    mean = 300
+    std_dev = 100
+    measurement = int(random.gauss(mean, std_dev))
+
+    return f"value={measurement}"
+
+
+def _generate_water_quality():
+    temperature = round(random.gauss(22, 6), 2)
+    ph = round(random.gauss(7, 2), 2)
+    turbidity = round(random.gauss(22, 6), 1)
+    dissolved_oxygen = round(random.gauss(8, 1.5), 1)
+    electrical_conductivity = int(random.gauss(412, 50))
+
+    return f"temperature={temperature},ph={ph},turbidity={turbidity},dissolved_oxygen={dissolved_oxygen},electrical_conductivity={electrical_conductivity}"
