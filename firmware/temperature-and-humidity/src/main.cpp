41d6e00e7fb6abd1fe9bdd86a6b97313234b4692
Daan Eggen <egdaan@gmail.com>
2026-04-09 19:48:42 +0200

    Added dht lib

--- firmware/temperature-and-humidity/src/main.cpp
+++ firmware/temperature-and-humidity/src/main.cpp
@@ -1,8 +1,26 @@
-#include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+
+#define DHTPIN 4
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Init");
+
+  dht.begin();
 }
 
-void loop() {}
+void loop() {
+  delay(2000);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("Temperature: "));
+  Serial.print(t);
+
+  Serial.println();
+}
